3422a7e4860ff5a801447e27f08ee9f6c7f94908
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-17 12:06:43 -0700

    Added displayNewGameButton and displayErrorMessage functions and conditional rendering

--- src/Components/GameBoardContainer/GameBoardContainer.js
+++ src/Components/GameBoardContainer/GameBoardContainer.js
@@ -1,14 +1,38 @@
 import GameBoard from '../GameBoard/GameBoard';
 import Keyboard from '../Keyboard/Keyboard';
+import newGameButton from '../../assets/new-game-button.png';
+import './GameBoardContainer.css'
 //create Loading component
 
-import '../GameBoard/GameBoard.css';
+const GameBoardContainer = ({ typeLetter, deleteLetter, typedLetters, currentWordInPlay, enterGuess, submittedWords, gameOver, error, startNewGame }) => {
+  const displayNewGameButton = () => {
+    return (
+      <div className='new-game-button-container'>
+        <img
+          className='new-game-button'
+          src={newGameButton}
+          alt="new game button"
+          onClick={() => startNewGame()}
+        />
+      </div>
+    );
+  }
 
-const GameBoardContainer = ({ typeLetter, deleteLetter, typedLetters, currentWordInPlay, enterGuess, submittedWords }) => {
-  
+  const displayErrorMessage = () => {
+    return (
+      <p className='error-message'>
+        {error}
+      </p>
+    );
+  }
+
   return (
     currentWordInPlay !== null ?
       <div className='game-board-container'>
+        <div className='game-board-header'>
+          {error ? displayErrorMessage() : null}
+          {gameOver ? displayNewGameButton() : null}
+        </div>
         <GameBoard
           typedLetters={typedLetters}
           submittedWords={submittedWords}
@@ -22,8 +46,9 @@ const GameBoardContainer = ({ typeLetter, deleteLetter, typedLetters, currentWor
           currentWordInPlay={currentWordInPlay}
         />
       </div>
-    :
+      :
       <div>Loading...</div>
   );
+  
 }
 export default GameBoardContainer;
