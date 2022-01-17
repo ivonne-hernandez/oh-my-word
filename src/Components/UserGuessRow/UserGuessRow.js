1d08b6625af10938b91a4f3c90342610956af335
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-16 18:42:11 -0700

    Add a UserGuessRow component

--- src/Components/UserGuessRow/UserGuessRow.js
+++ src/Components/UserGuessRow/UserGuessRow.js
@@ -1,27 +1,50 @@
 import './UserGuessRow.css';
-const UserGuessRow = ({ typedLetters, currentWordInPlay, submittedWords }) => {
+const UserGuessRow = ({ typedLetters, currentWordInPlay }) => {
+  const getLetterClass = (letter, letterIndex) => {
+    if (currentWordInPlay.includes(letter) && letter === currentWordInPlay[letterIndex]) {
+      return "green";
+    } else if (currentWordInPlay.includes(letter) && letter !== currentWordInPlay[letterIndex]) {
+      return "yellow";
+    } else if (!currentWordInPlay.includes(letter)) {
+      return "dark-grey";
+    }
+  }
+
+  const generateLetterTiles = () => {
+    const letterTiles = typedLetters.map((letter, letterIndex) => {
+      let letterClass = "typed-letter";
+
+      if (currentWordInPlay) {
+        letterClass = letterClass + " past-row " + getLetterClass(letter, letterIndex);
+      }
+
+      return (
+        <article
+          className={letterClass}
+          key={letterIndex}
+        >
+          {letter}
+        </article>
+      );
+    });
+
+    while (letterTiles.length < 5) {
+      const emptyLetterTile = (
+        <article
+          className="typed-letter"
+          key={letterTiles.length}
+        >
+        </article>
+      );
+      letterTiles.push(emptyLetterTile);
+    }
+
+    return letterTiles;
+  }
+
   return (
     <div className='user-guess-row-container'>
-      <article
-        className='typed-letter'>
-          {typedLetters[0]}
-      </article>
-      <article
-        className='typed-letter'>
-          {typedLetters[1]}
-      </article>
-      <article
-        className='typed-letter'>
-          {typedLetters[2]}
-      </article>
-      <article
-        className='typed-letter'>
-          {typedLetters[3]}
-      </article>
-      <article
-        className='typed-letter'>
-          {typedLetters[4]}
-      </article>
+      {generateLetterTiles()}
     </div>
   );
 }
