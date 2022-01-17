990d29ef3d194b2c5fdfdc83d51f1a0245a20953
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-16 17:42:36 -0700

    Add a GameBoard component

---
+++ src/Components/GameBoard/GameBoard.js
@@ -0,0 +1,54 @@
+import UserGuessRow from '../UserGuessRow/UserGuessRow';
+import './GameBoard.css';
+
+const GameBoard = ({ typedLetters, currentWordInPlay, submittedWords, currentRow }) => {
+  const generateRows = () => {
+    const rows = [];
+
+    const pastRows = submittedWords.map((word, index) => {
+      return (
+        <UserGuessRow 
+          className='past-row'
+          typedLetters={word}
+          key={index}
+        />
+      );
+    });
+    rows.push(...pastRows);
+
+    if (rows.length < 6) {
+      const activeRow = (
+        <UserGuessRow 
+          className="active-row"
+          typedLetters={typedLetters}
+          currentWordInPlay={currentWordInPlay}
+          key={rows.length}
+        />
+      );
+      rows.push(activeRow);
+    }
+    
+    while (rows.length < 6) {
+      const futureRow = (
+        <UserGuessRow
+          className="future-row"
+          typedLetters={["", "", "", "", ""]}
+          key={rows.length}
+        />
+      );
+      rows.push(futureRow);
+    }
+
+    return rows;
+  }
+  
+  return (
+    <div className='board-container'>
+      <main className="game-board">
+        {generateRows()}
+      </main>
+    </div>
+  );
+}
+
+export default GameBoard;
