5bc92b8645e9660bf9386eeab68727c61945cbad
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-18 08:39:20 -0700

    Remove the console log from the NotFound component

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,13 +1,15 @@
 import './NotFound.css';
 
 const NotFound = () => {
-  console.log(`not found`)
   return (
     <div className='invalid-route-container'>
-      <p className='page-not-found-message'>404: Page was not found.</p>
-      <p className='click-on-puzzle-message'>Click on the puzzle piece to navigate back to the homepage.
+      <p className='page-not-found-message'>
+        404: Page was not found.
+      </p>
+      <p className='click-on-puzzle-message'>
+        Click on the puzzle piece to navigate back to the homepage.
       </p>
     </div>
-  )
+  );
 }
 export default NotFound;
